248ee5beed15979add4f435111e4933632ee7576
vinay-sdet <vgarge@gmail.com>
2025-02-04 11:36:00 +0530

    First test case

--- tests/firstTestAPI.spec.ts
+++ tests/firstTestAPI.spec.ts
@@ -4,7 +4,7 @@ test.beforeEach(async({page})=>{
   await page.goto("https://conduit.bondaracademy.com/")
 })
 
-test('Has a text conduit', async ({ page }) => {
+test('Has a text conduit.', async ({ page }) => {
  
   // Expect a title "to contain" a substring.
   await expect(page.locator('.navbar-brand')).toHaveText('conduit');
